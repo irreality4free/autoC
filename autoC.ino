408b00dcbeaaf9a8947b496a806e6241f56f28e7
irreality4free <irreality4free@gmail.com>
2018-03-29 13:08:00 +0300

    working vers

--- autoC.ino
+++ autoC.ino
@@ -1,4 +1,4 @@
-#include <Wire.h>
+    #include <Wire.h>
 #include "Adafruit_INA219.h"
 
 Adafruit_INA219 sensor219;
@@ -8,7 +8,7 @@ Adafruit_INA219 sensor219;
 int relay_pin = 9;
 int M1_pin = 7;
 int M2_pin = 8;
-int button_pin = 4;
+int button_pin = 2;
 int button_val = 0;
 float busVoltage = 0;
 float current = 0; // Measure in milli amps
@@ -39,48 +39,78 @@ void MotorBackward() {
 
 void Run() {
 
-  busVoltage = sensor219.getBusVoltage_V();
-  current = sensor219.getCurrent_mA();
-  power = busVoltage * (current / 1000);
-
-  button_val = digitalRead(button_pin);
-  if (button_val == 1) {
-    PushSwitchON();
-    delay(2000);
-    MotorForward();
-    delay(500);
-    MotorStop();
-    PushSwitchOFF();
-  }
+  Serial.print("button state - ");
+  Serial.println(digitalRead(button_pin));
+if(digitalRead(button_pin)==0){
+  Serial.println("ATTENTION!!! Start rolling in 5 seconds>> BECAREFUL");
+  delay(1000);
+  Serial.println("4");
+  delay(1000);
+  Serial.println("3");
+  delay(1000);
+  Serial.println("2");
+  delay(1000);
+  Serial.println("1");
+  PushSwitchON();
+  Serial.println("Start Rolling");
+  delay(1000);
+  Roll();
+  Serial.println("Roll finished up in 5 seconds");
+  delay(1000);
+  Serial.println("4");
+  delay(1000);
+  Serial.println("3");
+  delay(1000);
+  Serial.println("2");
+  delay(1000);
+  Serial.println("1");
+  PushSwitchOFF();
+  delay(4000);
+}
 
 }
 
+void SerControl(){
+    if (Serial.available() > 0){
+    String command = Serial.readString();
+    if (command == "1\n"){
+      PushSwitchON();
+  }
+  if (command == "2\n"){
+  PushSwitchOFF();
+  }
+}
+}
+
+void Roll(){
+  MotorForward();
+delay(1000);
+while(1){
+current = sensor219.getCurrent_mA();
+
+Serial.println(current);
+if (current < -280){
+  MotorStop();
+  break;
+}
+}
+}
 
 void setup() {
   pinMode(relay_pin, OUTPUT);
   pinMode(M1_pin, OUTPUT);
   pinMode(M2_pin, OUTPUT);
   pinMode(button_pin, INPUT);
+  digitalWrite(button_pin, HIGH  );
   Serial.begin(9600);
   sensor219.begin();
-
+PushSwitchOFF();
 }
 
 
 
 void loop() {
-MotorForward();
-delay(1000);
-while(1){
-current = sensor219.getCurrent_mA();
 
-Serial.println(current);
-if (current < -400){
-  MotorStop();
-  while(1){
-    
-  }
-}
-}
-
+//Roll();
+//SerControl();
 }
